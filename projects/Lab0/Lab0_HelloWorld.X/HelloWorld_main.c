4ffa1a9e08e38e582147f69869ce79928dd9a18b
Jake Revino <jrevino@ucsc.edu>
2022-01-14 20:40:24 -0800

    working part 2

--- projects/Lab0/Lab0_HelloWorld.X/HelloWorld_main.c
+++ projects/Lab0/Lab0_HelloWorld.X/HelloWorld_main.c
@@ -14,14 +14,14 @@
 
 
 // define LED's
-#define LED1 TRISEbits.RE7
-#define LED2 TRISEbits.RE6
-#define LED3 TRISEbits.RE5
-#define LED4 TRISEbits.RE4
-#define LED5 TRISEbits.RE3  
-#define LED6 TRISEbits.RE2
-#define LED7 TRISEbits.RE1
-#define LED8 TRISEbits.RE0
+#define LED1 PORTEbits.RE7
+#define LED2 PORTEbits.RE6
+#define LED3 PORTEbits.RE5
+#define LED4 PORTEbits.RE4
+#define LED5 PORTEbits.RE3  
+#define LED6 PORTEbits.RE2
+#define LED7 PORTEbits.RE1
+#define LED8 PORTEbits.RE0
 
 // define buttons
 #define BTN1 PORTFbits.RF1
@@ -29,10 +29,10 @@
 #define BTN3 PORTDbits.RD6
 #define BTN4 PORTDbits.RD7
 
-#define PART1_buttons
+//#define PART1_buttons
 #define PART2_leds
 
-#define NOPS_FOR_5MS 5e-3
+#define NOPS_FOR_5MS 240000
 
 void main(void) {
     BOARD_Init();
@@ -40,14 +40,12 @@ void main(void) {
     TRISD = 0xff; // setting port D as input
     TRISF = 0xff; // setting port F as input
 
-    int i;
-    for (i = 0; i < NOPS_FOR_5MS; i++) {
-        asm(" nop ");
-
-    }
 #ifdef PART1_buttons 
     while (1) {
+      //  for (int i = 0; i < NOPS_FOR_5MS; i++) {
+        //    asm(" nop ");
 
+        //}
         if ((BTN1 == 1) || (BTN2 == 1) || (BTN3 == 1) || BTN4 == 1) {
             // TRISF = 0x04;
             LATE = 0b10100101; // only 4 LEDs will turn on 
@@ -57,12 +55,30 @@ void main(void) {
 #endif
 
 #ifdef PART2_leds
-  /*  while (1) {
+    
+    LED1 = 0;
+    LED2 = 0;
+    LED3 = 0;
+    LED4 = 0;
+    LED5 = 0;
+    LED6 = 0;
+    LED7 = 0;
+    LED8 = 0;
 
+    while (1) {
+        for (int j = 0; j < NOPS_FOR_5MS; j++) {
+            asm(" nop ");
 
-    } */
+        }
+        if ((BTN1 == 1) || (BTN2 == 1) || (BTN3 == 1) || BTN4 == 1) {
+            PORTE = 0x00;
+        }
+        if (PORTE > 0xff) {
+            PORTE = 0x00;
+        }
+        PORTE++;
+    }
 #endif
-
 
     while (1);
     BOARD_End();
