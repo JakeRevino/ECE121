51491047faf7ae12d9b9a3f89202370ab77a1cde
Jake Revino <jrevino@ucsc.edu>
2022-01-27 17:47:15 -0800

    changed enqueue and dequeue. Working on ISR

--- projects/Lab1/Lab1_Protocol_Communication.X/Protocol.c
+++ projects/Lab1/Lab1_Protocol_Communication.X/Protocol.c
@@ -1,6 +1,7 @@
 #include <BOARD.h>
 #include <xc.h>
 #include <Protocol.h>
+#include <sys/attribs.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -51,15 +52,23 @@ int Protocol_Init(void) {
      *                   URXDA - RX buffer data status
      */
 
-    U1MODE = 1; // Enable UART for 8-bit data
-    //IPC6bits.U1IP = 4; // sets interrupt priority
-    IFS0bits.U1TXIF = 0; // clear flags for UART1
+    U1MODE = 0x0000; // Enable UART for 8-bit data
+
+    // Interrupts configuration
+    IPC6bits.U1IP = 4; // sets interrupt priority
+    IPC6bits.U1IS = 0; // set interrupt subpriority
+    IEC0bits.U1TXIE = 1; // enable TX interrupts
+    IEC0bits.U1RXIE = 1; // enable RX interrupts
+    //IFS0bits.U1TXIF = 0; // clear interrupt flags for TX
+    //IFS0bits.U1RXIF = 0; // clear interrupt flags for RX
+
+
     U1STA = 0; // clear control registers
     U1BRG = ((Fpb / desired_baud) / 16) - 1; // baud rate gen value of 21 gives Baud Rate = 115k
 
 
-    IEC0bits.U1RXIE = 1; // enable RX interrupt
-    IEC0bits.U1TXIE = 1; // enable TX interrupt
+    //IEC0bits.U1RXIE = 1; // enable RX interrupt
+    //IEC0bits.U1TXIE = 1; // enable TX interrupt
 
     //IFS1 = 0; // clear flags to be safe
 
@@ -78,72 +87,91 @@ int Protocol_Init(void) {
 
 }
 
-static struct {
+typedef struct {
     unsigned char data[MAX_BUFFER_LENGTH];
-    unsigned int head;
-    unsigned int tail;
+    unsigned int head; // oldest data element
+    unsigned int tail; // next available free space
+    unsigned int size; // number of elements in buffer
 
 } CircleBuffer;
 
 //CircleBuffer circleBuffer;
 
-void init_buff(void) {
-    //CircleBuffer = {.head = 0, .tail = 0};
-    CircleBuffer.head = 0;
-    CircleBuffer.tail = 0;
-}
-
-char Protocol_IsMessageAvailable(void) {
-    if (CircleBuffer.head == CircleBuffer.tail) { // if head == tail then its empty
-        return TRUE;
-    } else
-        return FALSE;
-}
-
-char Protocol_IsQueueFull(void) {
-    if (CircleBuffer.head == ((CircleBuffer.tail + 1) % MAX_BUFFER_LENGTH)) {
-        return TRUE;
-    } else
-        return FALSE;
-}
-
-void enqueue_CB(unsigned char input) {
-    if (CircleBuffer.head == (CircleBuffer.tail + 1) % MAX_BUFFER_LENGTH) { // this is checking if its full
-        return;
-    } else {
-        CircleBuffer.data[CircleBuffer.tail] = input; // this is writing the data to the tail
-        CircleBuffer.tail = (CircleBuffer.tail + 1) % MAX_BUFFER_LENGTH; // this is incrementing 
+void init_buff(CircleBuffer * q) {
+    unsigned int i;
+    for (i = 0; i < MAX_BUFFER_LENGTH; i++) {
+        q->data[i] = 0;
+        q->head = 0;
+        q->tail = 0;
     }
 }
 
-char dequeue_CB(void) {
-    if (CircleBuffer.head == CircleBuffer.tail) {
-        return full_msg;
-    } else {
-        char temp_placeHolder;
-        temp_placeHolder = CircleBuffer.data[CircleBuffer.head];
-        CircleBuffer.head = (CircleBuffer.head + 1) % MAX_BUFFER_LENGTH;
-        return temp_placeHolder;
-    }
+int check_EmptyBuff(CircleBuffer * q) {
+    //if (CircleBuffer.head == CircleBuffer.tail) { // if head == tail then its empty
+    return q->size == 0;
 }
 
-/**
- * @Function char PutChar(char ch)
- * @param ch, new char to add to the circular buffer
- * @return SUCCESS or ERROR
- * @brief adds to circular buffer if space exists, if not returns ERROR
- * @author mdunne */
+int check_FullBuff(CircleBuffer * q) {
+    //if (CircleBuffer.head == ((CircleBuffer.tail + 1) % MAX_BUFFER_LENGTH)) {
+    return q->size == MAX_BUFFER_LENGTH;
+    //} else
+    //   return FALSE;
+}
+
+int enqueue_CB(CircleBuffer * q, unsigned char d) { // write to CB
+    if (!check_FullBuff(q)) {
+        q->data[q->tail++] = d;
+        q->tail %= MAX_BUFFER_LENGTH;
+        q->size++;
+        return 1; // success
+    } else
+        return 0; // fail
+
+    /*  if (CircleBuffer.head == (CircleBuffer.tail + 1) % MAX_BUFFER_LENGTH) { // this is checking if its full
+          return;
+      } else {
+          CircleBuffer.data[CircleBuffer.tail] = input; // this is writing the data to the tail
+          CircleBuffer.tail = (CircleBuffer.tail + 1) % MAX_BUFFER_LENGTH; // this is incrementing 
+      }*/
+}
+
+unsigned char dequeue_CB(CircleBuffer * q) { // read from CB
+    unsigned char temp = 0;
+    if (!check_EmptyBuff(q)) {
+        temp = q->data[q->head];
+        q->data[q->head++] = 0;
+        q->head %= MAX_BUFFER_LENGTH;
+        q->size--;
+        
+        
+        /* if (CircleBuffer.head == CircleBuffer.tail - 1) {
+             return full_msg;
+         } else {
+             char temp_placeHolder;
+             temp_placeHolder = CircleBuffer.data[CircleBuffer.head];
+             CircleBuffer.head = (CircleBuffer.head + 1) % MAX_BUFFER_LENGTH;
+             return temp_placeHolder;
+         } */
+    }
+    return temp;
+}
+
 int PutChar(char ch) {
-    if (Protocol_IsQueueFull() == TRUE) {
+    if (check_FullBuff()) {
         return ERROR;
     } else {
         U1TXREG = ch;
+        //enqueue_CB(ch);
         return SUCCESS;
     }
 
 }
 
-void __ISR(_UART1_VECTOR)IntUart1Handler(void);
+void __ISR(_UART1_VECTOR)IntUart1Handler(void) {
+
+
+
+}
 
 
 #ifdef TESTHARNESS
@@ -157,12 +185,17 @@ int main(void) {
 #ifdef PUTCHAR_TEST
     int i = 0;
     while (i <= sizeof (test_char)) {
+        //if (IFS0bits.U1TXIF == 1)
+
         PutChar(test_char[i]);
         i++;
+        if (U1STAbits.OERR) U1STAbits.OERR = 0; // reset overflow error
+        if (U1STAbits.PERR) U1STAbits.PERR = 0; // reset parity error
+        if (U1STAbits.FERR) U1STAbits.FERR = 0; // reset framing error
     }
-    
+
 #endif
-    
+
     while (1);
     BOARD_End();
     return 0;
