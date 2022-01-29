f783e4c3158d8181191628ac78c4015003aa33f7
Jake Revino <jrevino@ucsc.edu>
2022-01-28 22:53:05 -0800

    Working Interrupt driven TX

--- projects/Lab1/Lab1_Protocol_Communication.X/Protocol.c
+++ projects/Lab1/Lab1_Protocol_Communication.X/Protocol.c
@@ -6,15 +6,21 @@
 #include <stdlib.h>
 
 //#define MAIN_TEST
-#define Fpb 40000000
+#define Fpb 40e6 
 #define desired_baud 115200
+#define BaudRate ((Fpb / desired_baud) / 16) - 1; // baud rate gen value of 21 gives Baud Rate = 115k
 #define UART_TEST
+//#define putCharFlag
 #define BUFFER_TEST
 #define PUTCHAR_TEST
-#define MAX_BUFFER_LENGTH 640
+#define MAX_BUFFER_LENGTH 32
+//#define TESTHARNESS
+
+//#define LATEon LATE = 0x01
 
 // define LED's
-#define LED1 PORTEbits.RE7
+
+/*#define LED1 PORTEbits.RE7
 #define LED2 PORTEbits.RE6
 #define LED3 PORTEbits.RE5
 #define LED4 PORTEbits.RE4
@@ -23,36 +29,10 @@
 #define LED7 PORTEbits.RE1
 #define LED8 PORTEbits.RE0
 //char full_msg = 'F';
-
+ */
 int Protocol_Init(void) {
 
-    //    //Configuring the Mode registers
-    //    U1MODEbits.ON = 1; // UART is enabled and will be controlled by UEN & TXEN
-    //    U1MODEbits.IREN = 1; // Encoder & Decoder Enabled. Set = to 0 to disable
-    //    //U1MODEbits.FRZ = 1; // Freeze in debug mode enables. Set = 0 to disable
-    //    U1MODEbits.SIDL = 0; // Continue in idle mode. Set = 1 to make UART stop when it enters idle
-    //    U1MODEbits.RTSMD = 1; // UxRTS pin is in Simplex mode. Set = 0 for Flow Control Mode
-    //    U1MODEbits.UEN = 0b10; // U1TX, U1RX, ~(U1CTS-Clear To Send), and ~(U1RTS-Ready to Send) are enabled and used
-    //    U1MODEbits.WAKE = 1; // Wake Up is enables. Set = 0 to disable
-    //    U1MODEbits.LPBACK = 1; // Loopback Mode is enabled/ Set = 0 to disable;
-    //    U1MODEbits.ABAUD = 1; // Baud Rate measurement enabled. Set = 0 to disable
-    //    U1MODEbits.RXINV = 1; // RX idle state is '0'. Set = 0 to make idle state '1'
-    //    U1MODEbits.BRGH = 0; // Standard speed mode (16x Baud clk). Set = 1 for high speed mode (4x Baud clk enabled)
-    //    U1MODEbits.PDSEL = 0b00; // 8-bit data, No parity
-    //    U1MODEbits.STSEL = 0; // 1 Stop bit
 
-    //    // Configuring the Status and Control Registers
-    //    U1STA = 0; // Clear entire Status and Control Reg
-    //    U1STAbits.ADM_EN = 1; // Auto Address Detect Mode enabled. Set = 0 to disable
-    //    // ADDR - When ADM_EN = 1, this defines the address character to use for auto detect
-    //    U1STAbits.UTXISEL = 0b01; // Interrupt is generated when all characters have been transmitted
-    //    U1STAbits.UTXINV = 1; // Encoded UxTX idle state is '1'. This is tied to IREN!!!
-    //    U1STAbits.URXEN = 1; // Receiver is enabled
-    //    U1STAbits.UTXBRK = 1; // Send break on next TX, "Start-bit" followed by 12 zeros, followed by "stop-bit"
-    //    U1STAbits.UTXEN = 1; // Transmitter is enabled
-    //    U1STAbits.URXISEL = 0b10; // Interrupt occurs when buffer is 75% full
-    //    U1STAbits.OERR = 1; // Receive buffer has overflowed
-    //    
     /*
      Make sure to check: UTXBF reg status for state of TX buffer
      *                   TRMT reg to check if shift reg is empty
@@ -62,220 +42,161 @@ int Protocol_Init(void) {
      *                   URXDA - RX buffer data status
      */
 
-    U1MODE = 0x0000; // Enable UART for 8-bit data
-
-    // Interrupts configuration
-    IPC6bits.U1IP = 4; // sets interrupt priority
-    IPC6bits.U1IS = 0; // set interrupt subpriority
-    IEC0bits.U1TXIE = 1; // enable TX interrupts
-    IEC0bits.U1RXIE = 1; // enable RX interrupts
-    //IFS0bits.U1TXIF = 0; // clear interrupt flags for TX
-    //IFS0bits.U1RXIF = 0; // clear interrupt flags for RX
 
 
-    U1STA = 0; // clear control registers
-    U1BRG = ((Fpb / desired_baud) / 16) - 1; // baud rate gen value of 21 gives Baud Rate = 115k
+    U1STACLR = 0xff;
+    U1BRG = BaudRate;
 
-    U1MODEbits.UEN = 0;
-    U1STAbits.UTXISEL0 = 1;
-    U1STAbits.UTXISEL1 = 0;
+    U1MODEbits.PDSEL0 = 0;
+    U1MODEbits.PDSEL1 = 0; // sets 8-data and no parity
+    U1MODEbits.STSEL = 0; // sets 1 stop bit
+    U1MODEbits.UEN = 2;
+    //U1STAbits.UTXISEL = 0b10;
+    // U1STAbits.URXISEL = 0b11;
 
     // Configure to 8-N-1
-    U1MODEbits.PDSEL = 00; // sets 8-data and no parity
-    //U1MODEbits.PDSEL1 = 0;
-    U1MODEbits.STSEL = 0; // sets 1 stop bit
-    U1STAbits.UTXEN = 1; // enable Tx bits
-    U1STAbits.URXEN = 1; // enable Rx bits
+    IPC6bits.U1IP = 4;
+    IPC6bits.U1IS = 0;
+
+
+    IEC0bits.U1TXIE = 1;
+    U1STAbits.UTXISEL0 = 0;
+    U1STAbits.UTXISEL1 = 1;
+    U1STAbits.UTXEN = 1;
+    //U1STAbits.URXEN = 1; // enable Rx bits
     U1MODEbits.ON = 1; // Turn UART on
+
 
 
 }
 
 static struct {
-     char data[MAX_BUFFER_LENGTH];
-     int head; // oldest data element
-     int tail; // next available free space
-     int size; // number of elements in buffer
+    char data[MAX_BUFFER_LENGTH];
+    int head; // oldest data element
+    int tail; // next available free space
+    int size; // number of elements in buffer
+    int putCharFlag;
+    int collision;
 
 } CircleBuffer;
 
+/* These are some function prototypes */
 void init_buff(void);
-char check_EmptyBuff(void);
-char check_FullBuff(void);
+int check_EmptyBuff(void);
+int check_FullBuff(void);
 void enqueue_CB(char input);
 char dequeue_CB(void);
 
-//CircleBuffer circleBuffer;
+void init_buff(void) { // init the buffer
+    CircleBuffer.head = 0; // set head to 0
+    CircleBuffer.tail = 0; // set tail to 0
+    // CircleBuffer.size = 0; IDK if I need this
+    CircleBuffer.putCharFlag = 0;
 
-void init_buff(void) {
-    //CircleBuffer * q;
-    // unsigned int i;
-    //for (i = 0; i < MAX_BUFFER_LENGTH; i++) {
-    //CircleBuffer.data[i] = 0;
-    CircleBuffer.head = 0;
-    CircleBuffer.tail = 0;
-    CircleBuffer.size = 0;
-    // }
 }
 
-char check_EmptyBuff(void) {
+int check_EmptyBuff(void) {
     if (CircleBuffer.head == CircleBuffer.tail) { // if head == tail then its empty
-        return TRUE;
-    }
-    else
-        return FALSE;
+        return 1;
+    } else
+        return 0;
 }
 
-char check_FullBuff(void) {
+int check_FullBuff(void) {
     if (CircleBuffer.head == ((CircleBuffer.tail + 1) % MAX_BUFFER_LENGTH)) {
-        return TRUE;
-        } else
-           return FALSE;
-    }
-
+        return 1;
+    } else
+        return 0;
+}
 
 void enqueue_CB(char input) { // write to CB
-    if (!check_FullBuff()) {
+    if (!check_FullBuff()) { // check if there is space in the circle buffer
         CircleBuffer.data[CircleBuffer.tail] = input; // this is writing the data to the tail
         CircleBuffer.tail = (CircleBuffer.tail + 1) % MAX_BUFFER_LENGTH; // this is incrementing 
 
-       
+
     }
-    
+
 }
 
 char dequeue_CB(void) { // read from CB
     char temp;
-    if (!check_EmptyBuff()) {
+    if (!check_EmptyBuff()) { // check buffer isn't empty, ie. if there is data to dequeue
         temp = CircleBuffer.data[CircleBuffer.head];
-        // CircleBuffer.data[CircleBuffer.head++] = 0;
         CircleBuffer.head = (CircleBuffer.head + 1) % MAX_BUFFER_LENGTH;
-        //CircleBuffer.head %= MAX_BUFFER_LENGTH;
-        //CircleBuffer.size--;
         return temp;
-
-
-        /* if (CircleBuffer.head == CircleBuffer.tail - 1) {
-             return full_msg;
-         } else {
-             char temp_placeHolder;
-             temp_placeHolder = CircleBuffer.data[CircleBuffer.head];
-             CircleBuffer.head = (CircleBuffer.head + 1) % MAX_BUFFER_LENGTH;
-             return temp_placeHolder;
-         } */
     }
-    
+
 }
 
 int PutChar(char ch) {
-    if (!check_FullBuff()) {
-        enqueue_CB(ch);
+    if (!check_FullBuff()) { // check if the buffer is full
+    CircleBuffer.putCharFlag = 1;
+
+    
+        enqueue_CB(ch); // put the char on the buffer
+
+    }
+    CircleBuffer.putCharFlag = 0;
+
+    if ((U1STAbits.TRMT == 1) /*|| (CircleBuffer.collision == 1)*/) {
+        //CircleBuffer.collision = 0;
         IFS0bits.U1TXIF = 1;
         return SUCCESS;
+    }
 
-    } else
-        return ERROR;
+
+
+
+    return ERROR;
 
 }
 
 void __ISR(_UART1_VECTOR)IntUart1Handler(void) {
-    U1TXREG = dequeue_CB();
-    PORTE = 0b11111111;
-    IFS0bits.U1TXIF = 0;
+
+    if (IFS0bits.U1TXIF == 1) {
+
+        if (CircleBuffer.putCharFlag == 0) {
+            U1TXREG = dequeue_CB(); // value from CB goes into TX reg
+            IFS0bits.U1TXIF = 0;
+        }
+
+    }
 
 
+    //  IFS0bits.U1TXIF = 0; // clear interrupt flag
 
 }
 
-
 #ifdef TESTHARNESS
 
-int main(void) {
-    char test_char[] = " JAKE,_FUCK";
+int main() {
+    char test_char[] = "Alas! I think this is finally fookin workin!!! BITCH";
     BOARD_Init();
-    // TRISE = 0x00; // setting port E as output
     Protocol_Init();
-    //CircleBuffer TX_buff;
     init_buff();
+    //    PORTE = 0b11111111;
+    //    LATEon;
 
 #ifdef PUTCHAR_TEST
-    char i;
-    while (1) {
-    for (i = 1; i != '\0'; i++) {
-        //if (IFS0bits.U1TXIF == 1)
+    int i = 0;
+    // writeUART(test_char);
+    while (i < (sizeof test_char) - 1) {
+        // enqueue_CB(test_char);
 
-        PutChar(test_char[i]);
-        // PORTE = 0x11111111;
-       
-        //        if (U1STAbits.OERR) U1STAbits.OERR = 0; // reset overflow error
-        //        if (U1STAbits.PERR) U1STAbits.PERR = 0; // reset parity error
-        //        if (U1STAbits.FERR) U1STAbits.FERR = 0; // reset framing error
-    }
-    //IFS0bits.U1TXIF = 1;
+        if (U1STAbits.TRMT == 1) {
+            PutChar(test_char[i]);
+            i++;
+        }
     }
 
 #endif
 
     while (1);
     BOARD_End();
-    return 0;
+    //return 0;
 }
 #endif
-//#ifdef UART_TEST 
-//    while (1) {
-//    BOARD_Init();
-//    Protocol_Init();
-////    while (1) {
-////        U1TXREG = 'J';
-////    }
-//    //while(1);
-//    //BOARD_End();
-////#endif
-//    
-//#ifdef BUFFER_TEST
-//    unsigned char test_char[] = "PENIS ";
-//    init_buff();
-//    int length = sizeof(test_char);
-//    int i = 0;
-//    PutChar('H');
-//   /* while (i < length -1) {
-//        if (U1STAbits.TRMT) {
-//            U1TXREG = PutChar(test_char[i]);
-//            i++;
-//        
-//        
-//        }
-//    */ 
-//    
-//    
-//    //for (int i = 0; test_char[i] != EOF; i++) {
-//    //enqueue_CB(test_char[i]);
-//    
-//    
-//    //char char_to_put = 'K';
-//    
-//   // U1TXREG = (test_char[i]);
-//    
-//    //U1TXREG = test_char[i];
-//    }//dequeue_CB();
-//    //enqueue_CB(test_char[2]);
-//    dequeue_CB();
-//    dequeue_CB();
-//    //enqueue_CB(test_char[3]);
-//   // enqueue_CB(test_char[4]);
-//    //int full_or_nah = checkBuff_isFull();
-//    
-//#endif  
-//    //TX_Buff.buffer
-//  
-//    /*write a test for ur buffer*/
-//    
-//    
-//#endif
-//        BOARD_End();
-//
-//    
-//}
 
 
 
