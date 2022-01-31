eb540b4470711a709a1db239fe8bd0ab608bf56e
Jake Revino <jrevino@ucsc.edu>
2022-01-30 19:23:59 -0800

    Completed Protocol_SendMessage()

--- projects/Lab1/Lab1_Protocol_Communication.X/Protocol.c
+++ projects/Lab1/Lab1_Protocol_Communication.X/Protocol.c
@@ -19,16 +19,45 @@ unsigned char checkSum(char * payload);
 #define BaudRate ((Fpb / desired_baud) / 16) - 1; // baud rate gen value of 21 gives Baud Rate = 115k
 #define ECHO_TEST
 #define BUFFER_TEST
-//#define PUTCHAR_TEST
+#define PUTCHAR_TEST
 #define MAX_BUFFER_LENGTH 32
-#define CHECKSUM_TEST
+//#define CHECKSUM_TEST
 //#define TESTHARNESS
 char RXVAR;
 int collision;
+int putCharFlag = 0;
+
+/* Making structs for CB and Packet */
+static struct {
+    char data[MAX_BUFFER_LENGTH];
+    int head; // oldest data element
+    int tail; // next available free space
+    int size; // number of elements in buffer
+} CircleBuffer;
+
+int Protocol_SendMessage(unsigned char len, unsigned char ID, void *Payload) {
+    PutChar(HEAD);
+    unsigned char length = len + 1;
+    PutChar(length);
+    unsigned char checksum = ID;
+    PutChar(ID);
+    unsigned char i;
+    unsigned char *payload = (unsigned char*)Payload;
+    
+    for (i = 0; i <= length; ++i) {
+        PutChar(payload[i]);
+        checksum = checkSum(payload);
+    }
+    PutChar(TAIL);
+    PutChar(checksum);
+    PutChar('\r');
+    PutChar('\n');
+    
+}
 
 unsigned char checkSum(char * payload) {
     unsigned char checksum = 0;
-    int length = sizeof (payload) / sizeof (payload[0]);
+    int length = (sizeof (payload)) / (sizeof (payload[0]));
     int i;
     for (i = 0; i < (length - 1); i++) {
         checksum = (checksum >> 1) | (checksum << 7);
@@ -39,11 +68,10 @@ unsigned char checkSum(char * payload) {
 }
 
 int Protocol_Init(void) {
-    U1STACLR = 0xff;
+    U1STACLR = 0xffff;
     U1BRG = BaudRate;
 
-    U1MODEbits.PDSEL = 0b00;
-    // U1MODEbits.PDSEL1 = 0; // sets 8-data and no parity
+    U1MODEbits.PDSEL = 0b00; // sets 8-data and no parity
     U1MODEbits.STSEL = 0; // sets 1 stop bit
     U1MODEbits.UEN = 2; // enable UART
 
@@ -52,18 +80,16 @@ int Protocol_Init(void) {
 
     IEC0bits.U1TXIE = 1;
     U1STAbits.UTXISEL = 0b10;
-    // U1STAbits.UTXISEL1 = 1;
     U1STAbits.UTXEN = 1;
 
     IEC0bits.U1RXIE = 1;
     U1STAbits.URXISEL = 0b00;
-    //  U1STAbits.URXISEL1 = 0;
     U1STAbits.URXEN = 1;
 
     U1MODEbits.ON = 1; // Turn UART on
 
-     /*
-     Make sure to check: UTXBF reg status for state of TX buffer
+    /*
+    Make sure to check: UTXBF reg status for state of TX buffer
      *                   TRMT reg to check if shift reg is empty
      *                   RIDLE - Receiver Idle bit
      *                   PERR - Parity error status
@@ -72,23 +98,10 @@ int Protocol_Init(void) {
      */
 }
 
-static struct {
-    char data[MAX_BUFFER_LENGTH];
-    int head; // oldest data element
-    int tail; // next available free space
-    int size; // number of elements in buffer
-    int putCharFlag;
-    int collision;
-    //char RXVAR;
-
-} CircleBuffer;
-
 void init_buff(void) { // init the buffer
     CircleBuffer.head = 0; // set head to 0
     CircleBuffer.tail = 0; // set tail to 0
     // CircleBuffer.size = 0; IDK if I need this
-    CircleBuffer.putCharFlag = 0;
-    //CircleBuffer.RXVAR = 0;
 }
 
 int check_EmptyBuff(void) {
@@ -123,10 +136,10 @@ char dequeue_CB(void) { // read from CB
 
 int PutChar(char ch) {
     if (!check_FullBuff()) { // check if the buffer is full
-        CircleBuffer.putCharFlag = 1;
+        putCharFlag = 1;
         enqueue_CB(ch); // put the char on the buffer
     }
-    CircleBuffer.putCharFlag = 0;
+    putCharFlag = 0;
     if ((U1STAbits.TRMT == 1) || (collision == 1)) {
         collision = 0;
         IFS0bits.U1TXIF = 1;
@@ -138,11 +151,13 @@ int PutChar(char ch) {
 void __ISR(_UART1_VECTOR)IntUart1Handler(void) {
     if (IFS0bits.U1RXIF == 1) {
         IFS0bits.U1RXIF = 0;
+        LEDS_SET(0b11111111);
     }
     if (IFS0bits.U1TXIF == 1) {
-        if (CircleBuffer.putCharFlag == 0) {
+        if (putCharFlag == 0) {
             U1TXREG = dequeue_CB(); // value from CB goes into TX reg
             IFS0bits.U1TXIF = 0;
+            LEDS_SET(0b11001001);
         } else {
             collision = 1;
         }
@@ -158,12 +173,11 @@ int main() {
     BOARD_Init();
     Protocol_Init();
     init_buff();
-    
+    LEDS_INIT();
+
 #ifdef PUTCHAR_TEST
     int i = 0;
-    // writeUART(test_char);
     while (i < (sizeof test_char) - 1) {
-        //enqueue_CB(test_char[i]);
         if (U1STAbits.TRMT == 1) {
             PutChar(test_char[i]);
             i++;
