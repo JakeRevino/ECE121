f73257e26820411e0da0de154c4e5277b557f309
Jake Revino <jrevino@ucsc.edu>
2022-02-08 17:22:37 -0800

    Fixing Lab0 file path

--- projects/Lab0/Lab0_HelloWorld.X/build/default/production/_ext/943006391/BOARD.o.d
+++ projects/Lab0/Lab0_HelloWorld.X/build/default/production/_ext/943006391/BOARD.o.d
@@ -1,5 +1,5 @@
-build/default/production/_ext/319361124/BOARD.o: \
- ../../OneDrive/Desktop/ECE121/src/BOARD.c \
+build/default/production/_ext/943006391/BOARD.o: \
+ ../../Lab1/Lab1_Protocol_Communication.X/BOARD.c \
  C:/Users/jrev1/OneDrive/Desktop/ECE121/include/BOARD.h
 
 C:/Users/jrev1/OneDrive/Desktop/ECE121/include/BOARD.h:
